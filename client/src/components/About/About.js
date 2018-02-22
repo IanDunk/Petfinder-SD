8f9a2b35a40df04753cf25353011445aa4508cff
ematt44 <32374088+ematt44@users.noreply.github.com>
2018-02-21 18:12:08 -0800

    Update About.js

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -4,12 +4,24 @@ import "./About.css";
 const About = props => (
 
     <div className="panel panel-default">
-  <div className="panel-heading">
+  <div className="panel-heading"> 
+    
+    
     <h3 className="panel-title">About Us</h3>
   </div>
-  <div className="panel-body">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </div>
+ <div className="about-container">
+  Ask anyone who has adopted a pet and they’ll share with you their story of love, fun and companionship. Why? Because shelter pets are amazing!
+
+Within the next year, 29 million people just like you intend to bring a pet into their families. If fewer than 10 percent of them—just 2.4 million—choose to adopt, we will save all the dogs and cats who currently enter shelters but don’t find homes!
+
+Remember: Dogs and cats who are taken into the care of shelters and rescue groups each year find themselves homeless through no fault of their own; “moving” and “landlord issues” are the top reasons people give up their pets.
+
+This means shelters and rescues are full of loving, spayed or neutered, vaccinated and often trained pets who are just waiting to meet you! By adopting a cat or dog from a shelter or rescue, you can rest assured that you have not supported the puppy mill industry.
+
+Adopting a pet from a shelter or rescue is a win-win, for you and the pet. So, what are you waiting for? Begin your search and start your own story today!
+</div>
+
+  
 </div>
 )
 
